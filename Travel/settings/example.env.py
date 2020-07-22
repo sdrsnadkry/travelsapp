f7c1da9dc0879c30329e8f35ff7fceef4442df65
Sudarshan Adhikari <sdadkry95@gmail.com>
2020-07-22 20:32:39 +0545

    Demo Project

---
+++ Travel/settings/example.env.py
@@ -0,0 +1,13 @@
+from .base import *
+
+SECRET_KEY = ''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
